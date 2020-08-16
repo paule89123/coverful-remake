4cd1591857e6471c75b062095e2a635739a4ce5f
Paul Evans <paulevans89@gmail.com>
2020-08-16 16:45:39 +0100

    asid

--- src/templates/ProductPage/Carousel.js
+++ src/templates/ProductPage/Carousel.js
@@ -4,10 +4,10 @@ import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
+require('swiper/swiper.scss');
+require('swiper/components/navigation/navigation.scss');
+require('swiper/components/pagination/pagination.scss');
+require('swiper/components/scrollbar/scrollbar.scss');
 
 // install Swiper components
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);

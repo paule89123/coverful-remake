9d3bd2137443eeebc40901ab8b5ea6f6666a82ae
Paul Evans <paulevans89@gmail.com>
2020-10-10 23:48:11 +0100

    asd

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SEO from '~/components/seo'
 import HomepageBanner from '~/components/ProductGrid/HomepageBanner'
 import ProductGridHomepage from '~/components/ProductGrid/ProductGridHomepage'
+import arrow from '../components/Navigation/down-chevron.svg'
 
 // import banner from '../images/banner.jpeg'
 
@@ -21,10 +22,50 @@ const IndexPage = () => (
       ]}
     />
     <HomepageBanner />
-    <div style={{ fontSize: 48, marginTop: 96, fontFamily: 'AvenirBold' }}>
+    <div
+      style={{
+        fontSize: 40,
+        marginTop: 96,
+        marginBottom: 48,
+        fontFamily: 'AvenirBold',
+      }}
+    >
       New In
     </div>
-    <ProductGridHomepage />
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ width: '80%' }}>
+        <ProductGridHomepage />
+      </div>
+      <div
+        style={{
+          position: 'relative',
+          bottom: 28,
+          padding: '8px 29px 5px 16px',
+          border: '1px solid rgb(36,44,72)',
+          borderRadius: '50px',
+          color: 'rgb(36,44,72)',
+        }}
+      >
+        See more
+        <img
+          alt=""
+          style={{
+            transform: 'rotate(-90deg)',
+            height: 9,
+            marginLeft: 6,
+            marginTop: 4,
+            position: 'absolute',
+          }}
+          src={arrow}
+        />
+      </div>
+    </div>
   </div>
 )
 
